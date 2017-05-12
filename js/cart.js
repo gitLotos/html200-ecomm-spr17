f4e96e74b94cbaf7f9e2cf7010ad29d1a74a46f3
Brian Fox <fox.brian.m@gmail.com>
2017-05-11 19:44:59 -0700

    Adds remove from cart function

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,29 @@
 var cart = [];
+
+
 function AddtoCart(i)
 {
-  console.log("Your cart has exactly " + cart.push(products[i]) + " items");
+  console.log("Your cart has " + cart.push(products[i]) + " items");
+
+  for (i = 0; i < cart.length; ++i)
+  {
+    console.log(cart[i]);
+  }
+  return false;
+}
+
+function RemovefromCart(x)
+{
+  for (var i = 0; i < cart.length; i++)
+  {
+    if (cart[i].name === products[x].name)
+    {
+      console.log(cart[i].name + " has been removed from the cart");
+      cart.splice(i, 1);
+      console.log("Your cart has " + cart.length + " items");
+      break;
+    }
+  }
 
   for (i = 0; i < cart.length; ++i)
   {
